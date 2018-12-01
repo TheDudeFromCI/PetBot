13bfdca324bfa4efeae73137f7e5ea9aed81fc35
TheDudeFromCI <thedudefromci@gmail.com>
2018-12-01 03:23:27 -0700

    Added simple command test.

--- PetBot/src/main/java/me/ci/EventHandler.java
+++ PetBot/src/main/java/me/ci/EventHandler.java
@@ -1,7 +1,11 @@
 package me.ci;
 
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.events.Event;
 import net.dv8tion.jda.core.events.ReadyEvent;
+import net.dv8tion.jda.core.events.guild.GuildReadyEvent;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.EventListener;
 
 public class EventHandler implements EventListener
@@ -9,6 +13,46 @@ public class EventHandler implements EventListener
 	public void onEvent(Event event)
 	{
         if (event instanceof ReadyEvent)
-            System.out.println("API is ready!");
+        {
+        	handleOnReady();
+        	return;
+        }
+        
+        if (event instanceof GuildReadyEvent)
+        {
+        	handleGuildReady((GuildReadyEvent) event);
+        	return;
+        }
+        
+        if (event instanceof MessageReceivedEvent)
+        {
+        	handleMessageReceived((MessageReceivedEvent) event);
+        	return;
+        }
+	}
+	
+	private void handleOnReady()
+	{
+        System.out.println("API is ready!");
+	}
+	
+	private void handleGuildReady(GuildReadyEvent e)
+	{
+		e.getGuild().getDefaultChannel().sendMessage("Bot is connected.");
+	}
+	
+	private void handleMessageReceived(MessageReceivedEvent e)
+	{
+		if (e.getAuthor().isBot())
+			return;
+		
+		Message message = e.getMessage();
+		String content = message.getContentRaw();
+		
+		if (content.equals("!test1"))
+		{
+			MessageChannel channel = e.getChannel();
+			channel.sendMessage("Bot is listening.").queue();
+		}
 	}
 }
