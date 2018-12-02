2e269bdd4e2b9e80bd9dd3aa0741e70f3f3fb020
TheDudeFromCI <thedudefromci@gmail.com>
2018-12-02 13:26:27 -0700

    Help command now also displays available commands.

--- PetBot/src/main/java/me/ci/commands/list/HelpCommand.java
+++ PetBot/src/main/java/me/ci/commands/list/HelpCommand.java
@@ -5,10 +5,11 @@ import me.ci.commands.CommandEvent;
 import me.ci.commands.CommandHandler;
 import me.ci.commands.Subcommand;
 
-public class HelpCommand implements Command, Subcommand
+public class HelpCommand implements Command
 {
 	private Subcommand[] _subcommands = {
-			this
+			new HelpListSubcommand(),
+			new HelpCommandSubcommand()
 	};
 	private CommandHandler _commandHandler;
 	
@@ -29,67 +30,111 @@ public class HelpCommand implements Command, Subcommand
 		return _subcommands;
 	}
 
-	@Override
-	public int getArgumentCount()
+	private class HelpListSubcommand implements Subcommand
 	{
-		return 1;
-	}
-
-	@Override
-	public String getArgumentFormat(int index)
-	{
-		return "*";
-	}
-
-	@Override
-	public String getArgumentDisplay(int index)
-	{
-		return "[command]";
-	}
-
-	@Override
-	public String getDescription()
-	{
-		return "Displays help content for the requested command.";
-	}
-
-	@Override
-	public void run(CommandEvent com)
-	{
-		Command command = null;
-
-		String commandName = com.getArguments()[0];
-		for (Command c : _commandHandler.getCommands())
+		@Override
+		public int getArgumentCount()
 		{
-			if (c.getName().substring(1).equals(commandName))
+			return 1;
+		}
+
+		@Override
+		public String getArgumentFormat(int index)
+		{
+			return "list";
+		}
+
+		@Override
+		public String getArgumentDisplay(int index)
+		{
+			return "list";
+		}
+
+		@Override
+		public String getDescription()
+		{
+			return "Returns a list of all available commands.";
+		}
+
+		@Override
+		public void run(CommandEvent com)
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.append("Available commands:\n```\n");
+
+			for (Command c : _commandHandler.getCommands())
+				sb.append("!").append(c.getName());
+			
+			sb.append("```");
+			
+			com.sendMessage(sb.toString());
+		}
+	}
+
+	private class HelpCommandSubcommand implements Subcommand
+	{
+		@Override
+		public int getArgumentCount()
+		{
+			return 1;
+		}
+
+		@Override
+		public String getArgumentFormat(int index)
+		{
+			return "*";
+		}
+
+		@Override
+		public String getArgumentDisplay(int index)
+		{
+			return "[command]";
+		}
+
+		@Override
+		public String getDescription()
+		{
+			return "Returns all subcommands and a description of each.";
+		}
+
+		@Override
+		public void run(CommandEvent com)
+		{
+			Command command = null;
+
+			String commandName = com.getArguments()[0];
+			for (Command c : _commandHandler.getCommands())
 			{
-				command = c;
-				break;
+				if (c.getName().substring(1).equals(commandName))
+				{
+					command = c;
+					break;
+				}
 			}
-		}
-		
-		if (command == null)
-		{
-			com.sendMessage("Command not found!");
-			return;
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append("Available commands:\n```\n");
-		
-		for (Subcommand sub : command.getSubcommands())
-		{
-			sb.append(command.getName()).append(' ');
 			
-			for (int i = 0; i < sub.getArgumentCount(); i++)
-				sb.append(sub.getArgumentDisplay(i)).append(' ');
+			if (command == null)
+			{
+				com.sendMessage("Command not found!");
+				return;
+			}
 			
-			sb.append("\n");
+			StringBuilder sb = new StringBuilder();
+			sb.append("Available subcommands:\n```\n");
 			
-			sb.append("  ").append(sub.getDescription()).append("\n");
+			for (Subcommand sub : command.getSubcommands())
+			{
+				sb.append(command.getName()).append(' ');
+				
+				for (int i = 0; i < sub.getArgumentCount(); i++)
+					sb.append(sub.getArgumentDisplay(i)).append(' ');
+				
+				sb.append("\n");
+				
+				sb.append("  ").append(sub.getDescription()).append("\n");
+			}
+			
+			sb.append("```");
+			com.sendMessage(sb.toString());
 		}
-		
-		sb.append("```");
-		com.sendMessage(sb.toString());
 	}
 }
